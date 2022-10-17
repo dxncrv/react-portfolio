8eda16a7399becb46dcf1293f5302f5aacd143bd
dxncrv <doncorve@gmail.com>
2022-10-16 18:57:13 -0700

    revert test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,11 @@ const Hero = () => {
     >
       <Navbar />
       <h1 className="lg:w-3/5 transition-all duration-300 text-7xl my-20 sm:text-9xl sm:mx-16">
-        {/* I s
+        I s
         <span className="transition-color duration-500 hover:text-red-200">
           olve
         </span>
-        <br className="sm:block hidden " /> questions */}
-        Josh is awesome
+        <br className="sm:block hidden " /> questions
       </h1>
       <p className="xl:w-3/5 font-[Nunito] text-[1rem] md:text-[1.25rem] mx-12">
         I'm passionate about solving complex problems, and I do it by
